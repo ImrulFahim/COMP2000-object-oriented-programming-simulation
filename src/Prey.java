291b10bc622d407fe4c62307bbe0d3082b395953
ImrulFahim <imrulahsan.fahim@students.mq.edu.au>
2026-09-15 13:47:25 +1000

    Add multiple prey with fleeing, predation and population reproduction

--- src/Prey.java
+++ src/Prey.java
@@ -7,6 +7,55 @@ public class Prey extends Creature {
         super(x, y);
     }
 
+    public void flee(Predator predator, int width, int height) {
+
+        int xDifference = getX() - predator.getX();
+        int yDifference = getY() - predator.getY();
+
+        int distanceSquared =
+            xDifference * xDifference +
+            yDifference * yDifference;
+
+        int dangerDistance = 150;
+
+        if (distanceSquared < dangerDistance * dangerDistance) {
+
+            int fleeSpeed = 4;
+
+            if (getX() < predator.getX()) {
+                setX(getX() - fleeSpeed);
+            }
+
+            if (getX() > predator.getX()) {
+                setX(getX() + fleeSpeed);
+            }
+
+            if (getY() < predator.getY()) {
+                setY(getY() - fleeSpeed);
+            }
+
+            if (getY() > predator.getY()) {
+                setY(getY() + fleeSpeed);
+            }
+        }
+
+        if (getX() < 0) {
+            setX(0);
+        }
+
+        if (getY() < 0) {
+            setY(0);
+        }
+
+        if (getX() > width - getSize()) {
+            setX(width - getSize());
+        }
+
+        if (getY() > height - getSize()) {
+            setY(height - getSize());
+        }
+    }
+
     @Override
     public void draw(Graphics g) {
 
--- src/SimulationPanel.java
+++ src/SimulationPanel.java
@@ -3,45 +3,117 @@ import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Font;
+import java.util.ArrayList;
 
 public class SimulationPanel extends JPanel {
 
-    private Prey prey;
+    private ArrayList<Prey> preyList;
     private Predator predator;
 
     private int caughtCount = 0;
+    private int timerCount = 0;
 
     public SimulationPanel() {
 
-        prey = new Prey(100, 100);
-        predator = new Predator(500, 300);
+        preyList = new ArrayList<Prey>();
+
+        preyList.add(new Prey(100, 100));
+        preyList.add(new Prey(200, 400));
+        preyList.add(new Prey(350, 150));
+        preyList.add(new Prey(500, 400));
+        preyList.add(new Prey(650, 200));
+
+        predator = new Predator(400, 300);
 
         Timer timer = new Timer(20, e -> {
 
-            prey.move(
-                getWidth(),
-                getHeight()
-            );
+            timerCount = timerCount + 1;
 
-            predator.chase(
-                prey,
-                getWidth(),
-                getHeight()
-            );
+            // Move and flee
+            for (int i = 0; i < preyList.size(); i++) {
 
-            if (predator.catches(prey)) {
+                Prey prey = preyList.get(i);
 
-                caughtCount = caughtCount + 1;
+                prey.move(
+                    getWidth(),
+                    getHeight()
+                );
 
-                int newX =
-                    (int)(Math.random() *
-                    Math.max(1, getWidth() - 30));
+                prey.flee(
+                    predator,
+                    getWidth(),
+                    getHeight()
+                );
+            }
 
-                int newY =
-                    (int)(Math.random() *
-                    Math.max(1, getHeight() - 30));
+            // Only chase if prey still exist
+            if (preyList.size() > 0) {
 
-                prey = new Prey(newX, newY);
+                Prey closestPrey = preyList.get(0);
+
+                int closestDistance = Integer.MAX_VALUE;
+
+                // Find closest prey
+                for (int i = 0; i < preyList.size(); i++) {
+
+                    Prey currentPrey = preyList.get(i);
+
+                    int xDifference =
+                        predator.getX() - currentPrey.getX();
+
+                    int yDifference =
+                        predator.getY() - currentPrey.getY();
+
+                    int distance =
+                        xDifference * xDifference +
+                        yDifference * yDifference;
+
+                    if (distance < closestDistance) {
+
+                        closestDistance = distance;
+                        closestPrey = currentPrey;
+                    }
+                }
+
+                predator.chase(
+                    closestPrey,
+                    getWidth(),
+                    getHeight()
+                );
+
+                // Check if predator caught prey
+                for (int i = 0; i < preyList.size(); i++) {
+
+                    if (predator.catches(preyList.get(i))) {
+
+                        preyList.remove(i);
+
+                        caughtCount = caughtCount + 1;
+
+                        break;
+                    }
+                }
+            }
+
+            // New prey is born about every 5 seconds
+            if (timerCount >= 250) {
+
+                if (preyList.size() < 12) {
+
+                    int newX =
+                        (int)(Math.random() *
+                        Math.max(1, getWidth() - 30));
+
+                    int newY =
+                        (int)(Math.random() *
+                        Math.max(1, getHeight() - 30));
+
+                    preyList.add(
+                        new Prey(newX, newY)
+                    );
+                }
+
+                timerCount = 0;
             }
 
             repaint();
@@ -55,9 +127,15 @@ public class SimulationPanel extends JPanel {
 
         super.paintComponent(g);
 
-        prey.draw(g);
+        // Draw prey
+        for (int i = 0; i < preyList.size(); i++) {
+            preyList.get(i).draw(g);
+        }
+
+        // Draw predator
         predator.draw(g);
 
+        // Information
         g.setColor(Color.BLACK);
         g.setFont(new Font("Arial", Font.BOLD, 20));
 
@@ -65,6 +143,12 @@ public class SimulationPanel extends JPanel {
             "Prey caught: " + caughtCount,
             20,
             30
+        );
+
+        g.drawString(
+            "Prey alive: " + preyList.size(),
+            20,
+            55
         );
     }
 }
